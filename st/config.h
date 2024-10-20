b788d2639d40f881af9d3d4a857f8b35b0d77481
luigirad03 <igiuldj@gmail.com>
2024-10-20 09:46:48 +0200

    Added packages backup

--- st/config.h
+++ st/config.h
@@ -74,7 +74,7 @@ static unsigned int cursorthickness = 2;
 static int bellvolume = 0;
 
 /* default TERM value */
-char *termname = "xterm";
+char *termname = "screen-256color";
 
 /*
  * spaces per tab
@@ -95,63 +95,40 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-/*	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
-*/
-	/* 8 bright colors */
-/*	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
-*/
- 	[0] = "#282a2e", /* black   */
-  	[1] = "#a54242", /* red     */
-  	[2] = "#8c9440", /* green   */
-  	[3] = "#de935f", /* yellow  */
-  	[4] = "#5f819d", /* blue    */
-  	[5] = "#85678f", /* magenta */
-	[6] = "#5e8d87", /* cyan    */
-	[7] = "#707880", /* white   */
+
+  /* 8 normal colors */
+  [0] = "#282a2e", /* black   */
+  [1] = "#c9221e", /* red     */
+  [2] = "#adf182", /* green   */
+  [3] = "#ff8c28", /* yellow  */
+  [4] = "#9590bd", /* blue    */
+  [5] = "#523874", /* magenta */
+  [6] = "#5e8d87", /* cyan    */
+  [7] = "#b2acb5", /* white   */
 
   /* 8 bright colors */
-	[8]  = "#373b41", /* black   */
-	[9]  = "#cc6666", /* red     */
-	[10] = "#b5bd68", /* green   */
-	[11] = "#f0c674", /* yellow  */
-	[12] = "#81a2be", /* blue    */
-	[13] = "#b294bb", /* magenta */
-	[14] = "#8abeb7", /* cyan    */
-        [15] = "#c5c8c6", /* white   */
+  [8]  = "#373b41", /* black   */
+  [9]  = "#9e2224", /* red     */
+  [10] = "#c7fba5", /* green   */
+  [11] = "#ff8c28", /* yellow  */
+  [12] = "#d1c3f4", /* blue    */
+  [13] = "#916cad", /* magenta */
+  [14] = "#8abeb7", /* cyan    */
+  [15] = "#e9d5d3", /* white   */
 
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+  /* special colors */
+  [256] = "#010101", /* background */
+  [257] = "#e9d5d3", /* foreground */
 };
-
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 14;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 14;
+static unsigned int defaultrcs = 8;
 /*
  * Default shape of cursor
  * 2: Block ("█")
