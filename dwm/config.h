4955e6b8c8ec00ac44d18451f205a5db57eaf50b
Luigi Radano <igiuldj@gmail.com>
2025-11-29 12:54:40 +0000

    Updated dwm mute key

--- dwm/config.h
+++ dwm/config.h
@@ -7,7 +7,8 @@ static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
 static const unsigned int gappx = 8;    /* gap pixel between windows */
-static const char *fonts[] = {"monospace:pixelsize=24", "FontAwesome:pixelsize=8"};
+static const char *fonts[] = {"monospace:pixelsize=24",
+                              "FontAwesome:pixelsize=8"};
 static const char dmenufont[] = "monospace:size=28";
 static const char col_gray4[] = "#e9d5d3";
 static const char col_gray3[] = "#c9221e";
@@ -34,10 +35,12 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact = 0.55;     /* factor of master area size [0.05..0.95] */
-static const int nmaster = 1;        /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const float mfact = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster = 1;    /* number of clients in master area */
+static const int resizehints =
+    1; /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen =
+    1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
@@ -48,21 +51,24 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod4Mask
-#define TAGKEYS(KEY, TAG)                                        \
-  {MODKEY, KEY, view, {.ui = 1 << TAG}},                         \
-      {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}}, \
-      {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},          \
+#define TAGKEYS(KEY, TAG)                                                      \
+  {MODKEY, KEY, view, {.ui = 1 << TAG}},                                       \
+      {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}},               \
+      {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},                        \
       {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd)                                       \
-  {                                                      \
-    .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL } \
+#define SHCMD(cmd)                                                             \
+  {                                                                            \
+    .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL }                       \
   }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL};
+static char dmenumon[2] =
+    "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = {
+    "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
+    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
 static const char *termcmd[] = {"st", NULL};
 static const char *firefoxCmd[] = {"firefox", NULL};
 static const char *keyboardOn[] = {"onboard", NULL};
@@ -73,13 +79,19 @@ static const char *xrandrCmd[] = {"xrandr", "--auto", NULL};
 
 // static const char *upvol[]   = { "/usr/bin/setVolume.sh", "+5%",     NULL };
 // static const char *downvol[] = { "/usr/bin/setVolume.sh", "-5%",     NULL };
-static const char *downvol[] = {"/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL};
-static const char *upvol[] = {"/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL};
-static const char *mutevol[] = {"/usr/bin/pactl", "set-sink-mute", "0", "toggle", NULL};
-static const char *muteMic[] = {"/usr/bin/pactl", "set-source-mute", "@DEFAULT_SOURCE@", "toggle", NULL};
-static const char *downbright[] = {"/usr/bin/brightnessctl", "set", "10-", NULL};
+static const char *downvol[] = {"/usr/bin/pactl", "set-sink-volume",
+                                "@DEFAULT_SINK@", "-5%", NULL};
+static const char *upvol[] = {"/usr/bin/pactl", "set-sink-volume",
+                              "@DEFAULT_SINK@", "+5%", NULL};
+static const char *mutevol[] = {"/usr/bin/pactl", "set-sink-mute", "0",
+                                "toggle", NULL};
+static const char *muteMic[] = {"/usr/bin/pactl", "set-source-mute",
+                                "@DEFAULT_SOURCE@", "toggle", NULL};
+static const char *downbright[] = {"/usr/bin/brightnessctl", "set", "10-",
+                                   NULL};
 static const char *upbright[] = {"/usr/bin/brightnessctl", "set", "10+", NULL};
-static const char *playPause[] = {"/usr/bin/playerctl", "-a", "play-pause", NULL};
+static const char *playPause[] = {"/usr/bin/playerctl", "-a", "play-pause",
+                                  NULL};
 static const char *nextSong[] = {"/usr/bin/playerctl", "-a", "next", NULL};
 static const char *prevSong[] = {"/usr/bin/playerctl", "-a", "previous", NULL};
 static const char *screenshot[] = {"/usr/bin/screenshot.sh", NULL};
@@ -116,10 +128,11 @@ static const Key keys[] = {
     {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
     {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
     {0, XF86XK_AudioLowerVolume, spawn, {.v = downvol}},
-    {0, XF86XK_AudioMute, spawn, {.v = mutevol}},
+    {0, XF86XK_AudioMute, spawn, {.v = pavucontrol}},
     {0, XF86XK_AudioRaiseVolume, spawn, {.v = upvol}},
-    //	{ 0,                            XF86XK_MonBrightnessUp, 	   spawn,      {.v = upbright } },
-    //	{ 0,                            XF86XK_MonBrightnessDown, 	   spawn,    {.v = downbright } },
+    //	{ 0,                            XF86XK_MonBrightnessUp,
+    // spawn,      {.v = upbright } }, 	{ 0, XF86XK_MonBrightnessDown, spawn,
+    // {.v = downbright } },
     {MODKEY, XK_space, spawn, {.v = playPause}},
     {MODKEY, XK_Left, spawn, {.v = prevSong}},
     {MODKEY, XK_Right, spawn, {.v = nextSong}},
@@ -127,22 +140,18 @@ static const Key keys[] = {
     {MODKEY | ShiftMask, XK_d, spawn, {.v = nautilusCmd}},
     {MODKEY | ShiftMask, XK_p, spawn, {.v = xrandrCmd}},
 
-    //	{ 0,			                      XF86XK_AudioMicMute,	spawn,	{.v = muteMic	} },
-    TAGKEYS(XK_1, 0)
-        TAGKEYS(XK_2, 1)
-            TAGKEYS(XK_3, 2)
-                TAGKEYS(XK_4, 3)
-                    TAGKEYS(XK_5, 4)
-                        TAGKEYS(XK_6, 5)
-                            TAGKEYS(XK_7, 6)
-                                TAGKEYS(XK_8, 7)
-                                    TAGKEYS(XK_9, 8){MODKEY | ShiftMask | ControlMask, XK_q, quit, {0}},
+    //	{ 0,			                      XF86XK_AudioMicMute,
+    // spawn,	{.v = muteMic	} },
+    TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
+        TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
+            TAGKEYS(XK_9, 8){MODKEY | ShiftMask | ControlMask, XK_q, quit, {0}},
 };
 
 /* button definitions */
-/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
+ * ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
-    /* click                event mask      button          function        argument */
+    /* click                event mask      button          function argument */
     {ClkLtSymbol, 0, Button1, setlayout, {0}},
     {ClkLtSymbol, 0, Button3, setlayout, {.v = &layouts[2]}},
     {ClkWinTitle, 0, Button2, zoom, {0}},
